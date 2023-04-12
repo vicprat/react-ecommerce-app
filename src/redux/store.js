2908edb6fe84877069c4b6e909b0b2df288bd021
Victor Prado Trujillo <vicknan0182@gmail.com>
2023-04-12 14:02:34 -0600

    added redux persist to redux toolkit store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,28 +1,23 @@
-// import { compose, createStore, applyMiddleware } from 'redux'
 import { configureStore } from '@reduxjs/toolkit'
-// import { persistStore, persistReducer } from 'redux-persist'
-// import storage from 'redux-persist/lib/storage'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import logger from 'redux-logger'
 
 import { rootReducer } from './rootReducer'
 
-// const persistConfig = {
-//   key: 'root',
-//   storage,
-//   blacklist: ['user']
-// }
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user']
+}
 
-// const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean)
 
-// const composeEnhancers = (process.env.NODE_ENV !== 'production' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose
-
-// const composedEnhancers = composeEnhancers(applyMiddleware(...middleWares))
-
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(middleWares)
 })
 
-// export const persistor = persistStore(store)
+export const persistor = persistStore(store)
